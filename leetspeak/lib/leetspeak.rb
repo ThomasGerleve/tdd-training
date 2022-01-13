e28bad6ff21cfa55f304263c493bf501356d28ab
Thomas Gerleve <t.gerleve@web.de>
2022-01-13 14:04:31 +0100

    added basic specs and function; missing special cases

--- leetspeak/lib/leetspeak.rb
+++ leetspeak/lib/leetspeak.rb
@@ -1,3 +1,9 @@
-def leetspeak(input)
-
+class String
+  def leetspeak
+    self
+      .gsub('e', '3')
+      .gsub('o', '0')
+      .gsub('I', '1')
+      .gsub('s', 'z')
+  end
 end
--- leetspeak/spec/leetspeak_spec.rb
+++ leetspeak/spec/leetspeak_spec.rb
@@ -1,5 +1,14 @@
 require('rspec')
 require('leetspeak')
 
-describe('#leetspeak') do
+describe('String#leetspeak') do
+  it("returns String as is when no rules apply") do
+    expect("happy".leetspeak).to(eq("happy"))
+  end
+
+  it("returns String in leetspeak") do
+    expect("Don't you love these 'String' exercises? I do!".leetspeak)
+      .to(eq("D0n't y0u l0v3 th3z3 'String' 3x3rciz3z? 1 d0!"))
+  end
+
 end
