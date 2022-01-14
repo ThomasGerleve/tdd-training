2c83fe1127510ec207b43e06c61152c5f2decfff
Thomas Gerleve <t.gerleve@web.de>
2022-01-14 15:16:46 +0100

    initiated scrabble instance method

---
+++ 04. scrabble_score/lib/scrabble_score.rb
@@ -0,0 +1,9 @@
+class ScrabbleWord
+  def initialize(word)
+    @word = word
+  end
+
+  def scrabble
+    0
+  end
+end
--- 04. scrabble_score/spec/scrabble_score_spec.rb
+++ 04. scrabble_score/spec/scrabble_score_spec.rb
@@ -1,10 +1,15 @@
 require('rspec')
 require('scrabble_score')
 
-describe('') do
-
+describe ScrabbleWord do
+  describe '.scrabble' do
+    context 'given an empty string' do
+      it('should return zero') do
+        expect(ScrabbleWord.new('').scrabble).to eq(0)
+      end
+    end
+  end
 end
-
 
 # Word class initialized with a word
 # check each letter of the word with a hash and add their values
